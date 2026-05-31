b9762361368defdeff10f898ea2dedb1301725f2
Aditya Raj <araj05036@gmail.com>
2026-05-31 10:09:50 +0530

    Time: 4 ms (49.34%), Space: 69.6 MB (42.27%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,19 +1,16 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        // Calculate the sum of the first window
         double currentSum = 0;
         for (int i = 0; i < k; i++) {
             currentSum += nums[i];
         }
+        
         double maxSum = currentSum;
-        // Slide the window from index k to the end of the array
         for (int i = k; i < nums.length; i++) {
-            // Add the new element, remove the leftmost element of the previous window
             currentSum += nums[i] - nums[i - k];
-            // Keep track of the maximum sum found so far
             maxSum = Math.max(maxSum, currentSum);
         }
-        // The maximum average is the maximum sum divided by k
+        
         return maxSum / k;
     }
-}
+} 
