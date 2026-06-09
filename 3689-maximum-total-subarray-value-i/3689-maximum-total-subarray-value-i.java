3899ae0dabd99eb71a5f5dd92a8c653c357485c6
Aditya Raj <araj05036@gmail.com>
2026-06-09 20:49:40 +0530

    Time: 1 ms (100%), Space: 62.2 MB (66.91%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -3,16 +3,15 @@ class Solution {
         int maxVal = nums[0];
         int minVal = nums[0];
         
-        // Find the absolute max and min in the array
+    
         for (int num : nums) {
             if (num > maxVal) maxVal = num;
             if (num < minVal) minVal = num;
         }
         
-        // The best subarray is the one containing the global max and min
+    
         long diff = (long) maxVal - minVal;
         
-        // You can pick this exact same subarray k times
         return diff * k;
     }
 }
