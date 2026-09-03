db59ce0d27e2e95b24b70eb4a4063e1e03ad39c7
Aditya Raj <araj05036@gmail.com>
2026-09-03 23:34:08 +0530

    Time: 61 ms (69.78%), Space: 107.7 MB (33.3%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,19 +1,25 @@
-
-
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        int[] res = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>(); 
-
+        int[] answer = new int[n];
+        
+        // The stack will store the INDICES of the temperatures
+        Stack<Integer> stack = new Stack<>();
+        
         for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int prevIndex = stack.pop();
-                res[prevIndex] = i - prevIndex; 
+            int currentTemp = temperatures[i];
+            
+            // While stack is not empty AND current day is warmer than the day at the top of the stack
+            while (!stack.isEmpty() && currentTemp > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                // The number of days waited is the difference in indices
+                answer[prevDay] = i - prevDay; 
             }
+            
+            // Push the current day's index onto the stack
             stack.push(i);
         }
-
-        return res;
+        
+        return answer;
     }
 }
